b2fb5677620053689921a3a667c4658eebd126db
julio-salvador <julio.girardi@escola.pr.gov.br>
2025-06-02 14:05:03 +0000

    feito

--- exercicio02.py
+++ exercicio02.py
@@ -3,10 +3,12 @@
 #  Se alguma tarefa estiver com o tempo de entrega negativo, deve-se exibir uma mensagem de 
 # erro e não somar o total.
 
-projetoX = int(input("Digite o tempo de prazo para o projeto X: "))
-
-
-
-
-
-
+projetoX = int(input("Digite o tempo para o projeto X: "))
+projetoY = int(input("Digite o tempo para o projeto Y: "))
+projetoZ = int(input("Digite o tempo para o projeto Z: "))
+ 
+if projetoX < 0 or projetoY < 0 or projetoZ < 0:
+    print("Erro existe um número negativo.")
+else:
+    soma = projetoX + projetoY + projetoZ
+    print(f"Tempo total do projeto: {soma} dias")
