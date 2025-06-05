a50adefd6172b29293af6ddec220f76b18ec713b
julio-salvador <julio.girardi@escola.pr.gov.br>
2025-06-05 12:20:20 +0000

    nao feito

--- exercicio03.py
+++ exercicio03.py
@@ -1,6 +1,6 @@
 # Rafael trabalha com armazenamento de grãos e precisa garantir que a umidade do ar no local não ultrapasse 70%.
 # Escreva um programa que receba o valor da umidade atual e exiba um alerta se estiver acima do limite.
- 
+ umidade = int(input("Digite o valor da umidade do lugar"))
 
 
 
