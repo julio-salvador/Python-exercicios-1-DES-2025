c9acef7ace2d39ea495eea6b0e71ec164e71ddd9
julio-salvador <julio.girardi@escola.pr.gov.br>
2025-06-10 12:03:36 +0000

    FEITO

---
+++ matriz.py
@@ -0,0 +1,13 @@
+alunos = ["Adilson", "Bruno", "Carlos"]
+
+dias = ["seg", "ter", "qua", "qui"]
+
+reservas = [["Ausente" for _ in dias]  for _ in alunos]
+    print("Preencha com "S" para presença e "X" para ausência")
+
+
+
+
+
+
+
