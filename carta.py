2448c52faee9f0d4bd1b2d8c7c1fe0e6074aab18
julio-salvador <julio.girardi@escola.pr.gov.br>
2025-06-17 12:20:31 +0000

    nao terminado

---
+++ carta.py
@@ -0,0 +1,14 @@
+destinatario = input("Digite o nome do e-mail do destinatário: ")
+mensagem = input("Digite sua mensagem: ")
+remetente = input("Digite seu nome: ")
+data =input("Digite a data: ")
+
+print("\n seu E-mail")
+print(f"Olá {destinatario}, \nmensagem: \n\n{}")
+
+
+
+
+
+
+
--- exercicio06.py
+++ exercicio06.py
@@ -1,6 +1,10 @@
 # Bianca está programando o controle de acesso a uma plataforma que só funciona entre 9h e 21h.
 # O programa deve receber a hora atual (formato 24h) e informar se o acesso é permitido.
 
+hora = int(input("Digite a hora de agora. (24 horas): ")))
+if hora < 9 > 21:
+
+
 
 
 
