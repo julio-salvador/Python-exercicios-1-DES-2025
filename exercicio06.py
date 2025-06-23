0f83e0527d977b9a94ce09d07bc6030f0350aec0
julio-salvador <julio.girardi@escola.pr.gov.br>
2025-06-23 14:07:21 +0000

    arrumar

--- exercicio06.py
+++ exercicio06.py
@@ -1,11 +1,11 @@
 # Bianca está programando o controle de acesso a uma plataforma que só funciona entre 9h e 21h.
 # O programa deve receber a hora atual (formato 24h) e informar se o acesso é permitido.
 
-hora = int(input("Digite a hora de agora. (24 horas): ")))
+hora = int(input("Digite a hora de agora. (24 horas): "))
 if hora < 9 > 21:
-
-
-
+    print("A plataforma não pode ser aberta nesse horario.")
+else:
+    print("A plataforma poderá ser aberta neste momento.")
 
 
 
