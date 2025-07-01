566103e14be00af318b49e62f228720e6eb5b73f
julio-salvador <julio.girardi@escola.pr.gov.br>
2025-07-01 11:50:10 +0000

    feito

--- exercicio07.py
+++ exercicio07.py
@@ -5,11 +5,14 @@
 # Em treinamento (>=5 e <7)
 # Reprovado (<5)
 
+meta1 = int(input("Digite a média da meta 1: "))
+meta2 = int(input("Digite a média da meta 2: "))
+meta3 = int(input("Digite a média da meta 3: "))
+indice = meta1 + meta2 + meta3 / 3
 
-
-
-
-
-
-
-
+if indice >=7:
+    print("muito bem, vocẽ foi aprovado.")
+elif indice >=5 <7:
+    print("Vocẽ está fazendo o treinamento no treinamento.")
+else:
+    print("Uma pena, mas vocẽ não foi aprovado.")
