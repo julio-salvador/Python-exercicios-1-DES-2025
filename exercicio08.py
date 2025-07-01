6244da453d20418002cde323ce35fc0625a04781
julio-salvador <julio.girardi@escola.pr.gov.br>
2025-07-01 12:03:12 +0000

    feito

--- exercicio08.py
+++ exercicio08.py
@@ -4,4 +4,16 @@
 # De 51 a 150 km: R$ 15,00
 # Acima de 150 km: R$ 25,00
 
+distancia = int(input("Digite a distância percorrida: "))
+if distancia >= 50:
+    print("O preço pela distância é de R$ 5,00.")
+elif distancia >51 <150:
+    print("O preço pela distância é de R$ 15,00.")
+else:
+    print("O preço pela distância é de R$ 25,00.")
+
+
+
+ 
+
 
